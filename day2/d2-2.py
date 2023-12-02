ee548e098d735b8f98870eb959e4219a5c2f153e
kurkilmo <ilmo.kurki00@gmail.com>
2023-12-03 00:07:34 +0200

    day2

--- day2/d2-2.py
+++ day2/d2-2.py
@@ -4,25 +4,27 @@ for line in lines:
 	line = line.strip()
 	print(line)
 	draws = line.split(":")[1].split(";")
-	reds = []
-	blues = []
-	greens = []
+	reds = 0
+	blues = 0
+	greens = 0
 	for draw in draws:
 		spl = draw.split(",")
 		for c in spl:
 			try:
 				count = int(c.split(" ")[1])
-
 			except:
 				count = 0
 			if c.endswith("red"):
-				reds.append(count)
+				if count > reds:
+					reds = count
 			if c.endswith("green"):
-                                greens.append(count)
+				if count > greens:
+					greens = count
 			if c.endswith("blue"):
-                                blues.append(count)
-		print(f'red:{max(reds)} green:{max(greens)} blue:{max(blues)}')
-		pwr = max(reds)+max(greens)*max(blues)
-		print(pwr)
-		sum = sum + pwr
+				if count > blues:
+					blues = count
+	print(f'red:{reds} green:{greens} blue:{blues}')
+	pwr = reds*greens*blues
+	print(pwr)
+	sum = sum + pwr
 print(f'sum: {sum}')

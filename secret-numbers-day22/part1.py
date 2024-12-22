e84d8000e8c2c4c5a000bac09b98cc31dc127b37
kurkilmo <ilmo.kurki00@gmail.com>
2024-12-22 19:15:26 +0200

    d22p2 not working

--- secret-numbers-day22/part1.py
+++ secret-numbers-day22/part1.py
@@ -39,11 +39,40 @@ print(f"Part 1: sum: {secrets_sum}")
 # Part 2
 sequences = dict()
 
+def add(key, value):
+    try:
+        sequences[key].append(value)
+    except KeyError:
+        sequences[key] = [value]
+
+def price(secret:int):
+    return int(str(secret)[-1])
+
 for num in initial_nums:
     sequence = []
-    prev = 0
-    ch = num
-    for i in range(rounds):
-        #diff = 
+    
+    prev = num
+    for i in range(1,5):
+        secret = next_secret(prev)
+        diff = price(secret) - price(prev)
+        sequence.append(diff)
+        prev = secret
 
-        ch = next_secret(num)
+    add(tuple(sequence), price(secret))
+
+    for i in range(1, rounds):
+        secret = next_secret(prev)
+        diff = price(secret) - price(prev)
+        sequence.pop(0)
+        sequence.append(diff)
+        prev = secret
+
+        add(tuple(sequence), price(secret))
+
+max_price = max(list(map(sum, sequences.values())))
+print(sequences[(-2,1,-1,3)])
+for seq in sequences.keys():
+    s = sum(sequences[seq])
+    if s >= 23:
+        print(f"{seq}:{s}")
+print(f"Part 2: max bananas: {max_price}")
